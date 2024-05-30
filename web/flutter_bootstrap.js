babdf2086b7041b6bdc91867465b9a137f594960
ggstargame45 <ggstargame45@kookmin.ac.kr>
2024-05-31 06:04:12 +0900

    test

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "893959419"
+    serviceWorkerVersion: "3505062431"
   }
 });
